4eeb8a5a290d715d1f16def1560110d2ab53af7f
Chandradas <milton.chandradas@sap.com>
2022-11-03 15:54:49 -0400

    hana db dependency...

--- node/server.js
+++ node/server.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const xsenv = require("@sap/xsenv");
+const hdbext = require("@sap/hdbext");
+
+let services = xsenv.getServices({ hana: "myawesomehana-db" });
 
 const app = express();
+app.use("/hana", hdbext.middleware(services.hana));
 
 const PORT = process.env.PORT || 5000;
 console.log("PORT: ", PORT);
 
+app.get("/hana", (req, res, next) => {
+  let sql = "SELECT * FROM DUMMY";
+  req.db.exec(sql, (err, rows) => {
+    if (err) return next(err);
 
-app.get("/hello", (req, res) => res.send("Hello from express..."));
+    res.send(rows);
+  });
+});
 
 app.listen(PORT, console.log(`Listening on ${PORT}`));
